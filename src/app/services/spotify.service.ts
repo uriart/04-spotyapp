f5804a4917a1acece3d6b021549bcab8d7751ccc
Uriart <44235989+uriart@users.noreply.github.com>
2020-10-12 21:57:31 +0200

    Update spotify.service.ts

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,7 +13,7 @@ export class SpotifyService {
    getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQA9dtSV9S6En4PETgwFC3EyMKGsXqGERtrmZC5GIztIGrlwKpN6uWbrTlL4lkO9aG6SLDBaIWt5myX3OqI'
+      'Authorization': 'Bearer REMPLAZAR_TOKEN_AQUÍ'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
