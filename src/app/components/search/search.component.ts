8def98dc09c643ee455b692dbec56dbe83609203
uriart <loiro_8@hotmail.com>
2020-10-12 21:50:14 +0200

    first commit

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styles: [
+  ]
+})
+export class SearchComponent {
+
+  artistas: any[] = [];
+
+  constructor(private spotify: SpotifyService) { }
+
+  buscar(termino: string) {
+    console.log(termino);
+    this.spotify.getArtistas( termino )
+      .subscribe( (data: any) => {
+        console.log(data.artists.items);
+        this.artistas = data.artists.items;
+      });
+  }
+
+}
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify service listo');
+   }
+
+   getNewReleases() {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQA9dtSV9S6En4PETgwFC3EyMKGsXqGERtrmZC5GIztIGrlwKpN6uWbrTlL4lkO9aG6SLDBaIWt5myX3OqI'
+    });
+
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+
+   }
+
+   getArtistas( termino: string ) {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQA9dtSV9S6En4PETgwFC3EyMKGsXqGERtrmZC5GIztIGrlwKpN6uWbrTlL4lkO9aG6SLDBaIWt5myX3OqI'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=20`, { headers });
+
+   }
+
+}
